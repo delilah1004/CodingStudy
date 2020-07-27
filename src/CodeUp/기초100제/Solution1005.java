9dd23fe0c2b9f9864152b92a0acfe3e5fd5936cf
Delilah1004 <pskdy8414@naver.com>
2020-07-27 23:42:51 +0900

    출력하기

---
+++ src/CodeUp/기초100제/Solution1005.java
@@ -0,0 +1,5 @@
+public class Solution1005 {
+  public static void main(String args[]) {
+      System.out.print("\"Hello World\"");
+  }
+}
---
+++ src/CodeUp/기초100제/Solution1006.java
@@ -0,0 +1,5 @@
+public class Solution1002 {
+  public static void main(String[] args) {
+    System.out.print("\"!@#$%^&*()\"");
+  }
+}
